7a5557ae1920577d6fcfc5e1f34b5526b888b749
Ram <ghotekarram7@gmail.com>
2024-11-29 00:54:39 +0530

    Experimenting with Calculator

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,19 @@
 public class HelloWorld
 {
+    public int add(int i, int j)
+    {
+        return i+j;
+    }
     public static void main(String[] args)
     {
         System.out.println("Hello World");
         System.out.println();
+
+        HelloWorld obj= new HelloWorld();
+        int result = obj.add(4,3);
+        System.out.println(result);
+
     }
+
+
 }
